9daf9d86879df5c87a9d3172273730aa26629742
wahyu608 <wrusdiarsa608@gmail.com>
2024-03-09 10:29:41 +0800

    add const to improve peformance

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,13 +15,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +29,8 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.blue, 
         foregroundColor: Colors.white, 
-        title: Text('Appbar'),
-        actions: [
+        title: const Text('Appbar'),
+        actions: const [
           Icon(Icons.notifications),
           SizedBox(
             width: 10,
@@ -41,105 +41,108 @@ class MyHomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView(
-        
-        children: [
-          Center(
-            child: Text(
-              'Hello world', 
-              style: TextStyle(
-                fontWeight: FontWeight.bold, 
-                fontSize: 20,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Center(
+              child: Text(
+                'Hello world', 
+                style: TextStyle(
+                  fontWeight: FontWeight.bold, 
+                  fontSize: 20,
+                ),
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                height: 100, 
-                width: 100, 
-                color: Colors.amber,
-              ),
-              Container(
-                height: 100, 
-                width: 100, 
-                color: Colors.deepPurple,
-              ),
-              Container(
-                height: 100, 
-                width: 100, 
-                color: Colors.deepOrangeAccent,
-              ),
-              Container(
-                height: 100, 
-                width: 100, 
-                color: Colors.green,
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Image.network(
-            'https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-            width: 300,
-            fit: BoxFit.contain,
-          ),
-          SizedBox(height: 20),
-           SizedBox(height: 20),
-            SizedBox(height: 20),
-             SizedBox(height: 20),
-          Image.asset(
-            'assets/images/kucing.jpeg',
-            height: 300,
-            width: 300,
-            fit: BoxFit.contain,
-          ),
-          SizedBox(height: 20), 
-          TextButton(
-            onPressed: () {
-              
-            },
-            child: Text(
-              'Text Button',
-              style: TextStyle(
-                color: Colors.blue,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  height: 100, 
+                  width: 100, 
+                  color: Colors.amber,
+                ),
+                Container(
+                  height: 100, 
+                  width: 100, 
+                  color: Colors.deepPurple,
+                ),
+                Container(
+                  height: 100, 
+                  width: 100, 
+                  color: Colors.deepOrangeAccent,
+                ),
+                Container(
+                  height: 100, 
+                  width: 100, 
+                  color: Colors.green,
+                ),
+              ],
             ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white,
+            const SizedBox(height: 20),
+            Image.network(
+              'https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+              width: 200,
+              fit: BoxFit.contain,
             ),
-            onPressed: () {
-              
-            },
-            child: Text('Elevated Button'),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white,
-            ),
-            onPressed: () {
-          
-            },
-            child: Text('Filled Button'),
-          ),
+            const SizedBox(height: 20),
 
-          SizedBox(height: 20), 
-          Container(
-            child: ListTile(
-              leading: Icon(Icons.notifications),
-              title: Text('title'),
-              subtitle: Text('subtitle'),
-              trailing: Icon(Icons.access_time_outlined),
+            Image.asset(
+              'assets/images/kucing.jpeg',
+              height: 400,
+              width: 300,
+              fit: BoxFit.contain,
+            ),
+            const SizedBox(height: 20), 
+            const SizedBox(height: 20),
+            const SizedBox(height: 20),
+            const SizedBox(height: 20),
+            const SizedBox(height: 20),
+            const SizedBox(height: 20),
+
+            TextButton(
+              onPressed: () {
+                
+              },
+              child: const Text(
+                'Text Button',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {
+                
+              },
+              child: const Text('Elevated Button'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {
+              
+              },
+              child: const Text('Filled Button'),
+            ),
+
+            const SizedBox(height: 20), 
+            ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('title'),
+              subtitle: const Text('subtitle'),
+              trailing: const Icon(Icons.access_time_outlined),
               tileColor: Colors.grey.shade200,
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
